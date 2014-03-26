f49031b45a649b7c805747b9d8897872c576d042
ChrisBAshton <chrisashtonweb@gmail.com>
2014-03-26 15:09:37 +0000

    made variables local

--- Website/Radioactive-Evolution/js/app/module/controller/audio_controller.js
+++ Website/Radioactive-Evolution/js/app/module/controller/audio_controller.js
@@ -2,31 +2,13 @@ define(['bootstrap'], function (bs) {
 
     var AudioController = function () {
 
-        var audioAssets,
-            currentAsset,
-            crunch;
-
         this.init = function () {
-            crunch = document.createElement('audio');
+            var crunch = document.createElement('audio');
             crunch.setAttribute('src', "sounds/crunch.wav");
-            success = document.createElement('audio');
+            var success = document.createElement('audio');
             success.setAttribute('src', "sounds/success.wav");
-            death = document.createElement('audio');
+            var death = document.createElement('audio');
             death.setAttribute('src', "sounds/death.wav");
-            
-            // audioAssets = {
-            //     "crunch": crunch,
-            //     "success": success
-            // };
-
-            // // @TODO - tidy up. Audio should respond to events, not be triggered
-            // // manually in this way.
-            // bs.pubsub.addListener('regame:sound:play', function (asset) {
-            //     currentAsset = audioAssets[asset];
-            //     currentAsset.currentTime = 0;
-            //     currentAsset.play();
-            // });   
-
 
             bs.pubsub.addListener('regame:action:user_died', function () {
                 death.play();
