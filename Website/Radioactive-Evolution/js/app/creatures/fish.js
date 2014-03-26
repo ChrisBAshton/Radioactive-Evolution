dde7195a595d49cdb3dadadd34c25fdcfb64f9f0
ChrisBAshton <chrisashtonweb@gmail.com>
2014-03-26 16:19:55 +0000

    separating god function into smaller functions

--- Website/Radioactive-Evolution/js/app/creatures/fish.js
+++ Website/Radioactive-Evolution/js/app/creatures/fish.js
@@ -1,4 +1,4 @@
-define(['bootstrap', 'creatures/_creature', 'creatures/user'], function (bs, Creature, user) {
+define(['bootstrap', 'creatures/_creature', 'creatures/user', '_helpers/object_helper'], function (bs, Creature, user, objects) {
 
 	/**
 	* Defines a fish.
@@ -39,19 +39,18 @@ define(['bootstrap', 'creatures/_creature', 'creatures/user'], function (bs, Cre
 				this.eyecolor = randomElement(colors);
 			} while (this.eyecolor == this.color);
 			
-			// position
-			var spawningOnUser = true;
+			// @TODO - implement Chris 2014.
+			var spawningOnUser = false;//true;
 			do {
 				// give the fish some coordinates
 				this.y = Math.floor(Math.random() * (bs.config.canvas.height-bs.config.canvas.elements.water-(bs.config.canvas.height-bs.config.canvas.elements.sand)))+bs.config.canvas.elements.water;
 				var dir = Math.random();	// come from right or left, 50/50 chance
 				this.x = (dir > 0.5) ? 0:bs.config.canvas.width;
 				// check if fish has spawned on top of user
-				// TODO - fix. Chris 2014
-				// if(!level.collision(this, user)) {
-				// 	// fish hasn't spawned on user, so these coordinates are fine
-				spawningOnUser = false;
-				// }	
+				if(!objects.collide(self, user)) {
+					// fish hasn't spawned on user, so these coordinates are fine
+					spawningOnUser = false;
+				}	
 			} while(spawningOnUser);
 			// miscellaneous
 			this.direction = (this.x == 0) ? "right":"left";
@@ -100,91 +99,119 @@ define(['bootstrap', 'creatures/_creature', 'creatures/user'], function (bs, Cre
 		* @method move
 		*/
 		this.move = function() {
-			// only let the fish move if it's alive- obviously!
-			if(this.alive) {
-				// check user is near fish. This is the beauty of x,y being the CENTRAL coordinates
-				if( !user.camouflaged() && (Math.abs(this.x - user.getX()) < this.range) && (Math.abs(this.y - user.getY()) < this.range) ) {
-				
-					// fish is near the user
-					
-					if((this.width * this.height) > (user.getWidth() * user.getHeight())) {
-						// fish is bigger than the user and will hunt him down!
-						
-						if( Math.abs(this.x - user.getX()) <= this.speed) {
-							// x-axis is sufficiently close. 
-							// Set to user's x to avoid "jittery" effect caused by this.speed being more than the difference
-							this.x = user.getX();
-						} else if(this.x < user.getX()) {
-							this.direction = "right";
-							this.x += this.speed;
-						} else if(this.x > user.getX()) {
-							this.direction = "left";
-							this.x -= this.speed;
-						}
-						
-						if( Math.abs(this.y - user.getY()) <= this.speed) {
-							// y-axis is sufficiently close. set to user's y to avoid "jittery" effect.
-							this.y = user.getY();
-						} else if(this.y < user.getY()) {
-							this.y += this.speed;
-						} else if(this.y > user.getY()) {
-							this.y -= this.speed;
-						}
-					} else {
-						// user is bigger than the fish, so fish will swim away
-						if(this.x < user.getX()) {
-							this.direction = "left";
-							this.x -= this.speed;
-						} else if(this.x > user.getX()) {
-							this.direction = "right";
-							this.x += this.speed;
-						} else {
-							// fish is at same x level as user and will escape in the direction it is travelling
-							if(this.direction == "left") {
-								this.x -= this.speed;
-							} else {
-								this.x += this.speed;
-							}
-						}
-						
-						if(this.y < user.getY()) {
-							this.y -= this.speed;
-						} else if(this.y >= user.getY()) {
-							this.y += this.speed;
-						}
-					}
-				} else {
-					// fish hasn't seen the user so will continue on its journey
-					if(this.direction == "left") {
-						this.x -= this.speed;
-					} else {
-						this.x += this.speed;
-					}
-				}
-				
-				// do some limit checking to ensure fish stays within confines of the water
-				if(this.x <= 0) {
-					this.direction = "right";
-					this.x = 0;
-				} else if(this.x >= bs.config.canvas.width) {
-					this.direction = "left";
-					this.x = bs.config.canvas.width;
-				}
-				// if fish has followed user "above" water level (user has flying fish ability) then force fish below water level
-				if(this.y <= bs.config.canvas.elements.water) {
-					this.y = bs.config.canvas.elements.water;
-				} else if(this.y >= bs.config.canvas.elements.sand) {
-					this.y = bs.config.canvas.elements.sand;
-				}
+			if (self.alive) {
+				makeFishSwim();
 			} else {
-				// fish is dead- make it float to the surface
-				if(this.y < bs.config.canvas.elements.water) {
-					this.y++;
-				} else if(this.y > bs.config.canvas.elements.water) {
-					this.y--;
-				}
+				makeCarcasFloatToSurface();
 			}
 		}
+
+		var makeFishSwim = function () {
+			if (fishCanSeeUser()) {
+				reactToTheUser();
+			} else {
+				swimBackAndForth();
+			}
+			
+			stayInsideThePond();
+		};
+
+		var makeCarcasFloatToSurface = function () {
+			if(self.y < bs.config.canvas.elements.water) {
+				self.y++;
+			} else if(self.y > bs.config.canvas.elements.water) {
+				self.y--;
+			}
+		};
+
+		// check user is near fish. This is the beauty of x,y being the CENTRAL coordinates
+		var fishCanSeeUser = function () {
+			return !user.camouflaged() && (Math.abs(self.x - user.getX()) < self.range) && (Math.abs(self.y - user.getY()) < self.range);
+		};
+
+		var reactToTheUser = function () {
+			if(fishIsBiggerThanUser()) {
+				huntUserDown();
+			} else {
+				swimAwayFromUser();
+			}
+		};
+
+		var swimBackAndForth = function () {
+			// fish hasn't seen the user so will continue on its journey
+			if(self.direction == "left") {
+				self.x -= self.speed;
+			} else {
+				self.x += self.speed;
+			}
+		};
+
+		var stayInsideThePond = function () {
+			// do some limit checking to ensure fish stays within confines of the water
+			if(self.x <= 0) {
+				self.direction = "right";
+				self.x = 0;
+			} else if(self.x >= bs.config.canvas.width) {
+				self.direction = "left";
+				self.x = bs.config.canvas.width;
+			}
+			// if fish has followed user "above" water level (user has flying fish ability) then force fish below water level
+			if(self.y <= bs.config.canvas.elements.water) {
+				self.y = bs.config.canvas.elements.water;
+			} else if(self.y >= bs.config.canvas.elements.sand) {
+				self.y = bs.config.canvas.elements.sand;
+			}
+		};
+
+		var fishIsBiggerThanUser = function () {
+			return (self.width * self.height) > (user.getWidth() * user.getHeight());
+		};
+
+		var huntUserDown = function () {
+			if( Math.abs(self.x - user.getX()) <= self.speed) {
+				// x-axis is sufficiently close. 
+				// Set to user's x to avoid "jittery" effect caused by self.speed being more than the difference
+				self.x = user.getX();
+			} else if(self.x < user.getX()) {
+				self.direction = "right";
+				self.x += self.speed;
+			} else if(self.x > user.getX()) {
+				self.direction = "left";
+				self.x -= self.speed;
+			}
+			
+			if( Math.abs(self.y - user.getY()) <= self.speed) {
+				// y-axis is sufficiently close. set to user's y to avoid "jittery" effect.
+				self.y = user.getY();
+			} else if(self.y < user.getY()) {
+				self.y += self.speed;
+			} else if(self.y > user.getY()) {
+				self.y -= self.speed;
+			}
+		};
+
+		var swimAwayFromUser = function () {
+			if (self.x < user.getX()) {
+				self.direction = "left";
+				self.x -= self.speed;
+			} else if (self.x > user.getX()) {
+				self.direction = "right";
+				self.x += self.speed;
+			} else {
+				// fish is at same x level as user and will escape in the direction it is travelling
+				if(self.direction == "left") {
+					self.x -= self.speed;
+				} else {
+					self.x += self.speed;
+				}
+			}
+			
+			if(self.y < user.getY()) {
+				self.y -= self.speed;
+			} else if(self.y >= user.getY()) {
+				self.y += self.speed;
+			}
+		};
 
 		self.reset();
 	};
