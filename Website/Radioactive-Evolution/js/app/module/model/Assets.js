2b8c9869b7c92e8f1ee8e45b6db14378269d28f2
ChrisBAshton <chrisashtonweb@gmail.com>
2014-03-26 15:43:12 +0000

    removed sounds from assets.js

--- Website/Radioactive-Evolution/js/app/module/model/Assets.js
+++ Website/Radioactive-Evolution/js/app/module/model/Assets.js
@@ -1,9 +1,5 @@
 define(['_helpers/browser', 'bootstrap'], function (browser, bs) {
 
-    /**
-    *   A script for loading the required sounds and images
-    */
-
     // images
     var img_tick = new Image();
     img_tick.src = "images/tick.png";
@@ -23,47 +19,6 @@ define(['_helpers/browser', 'bootstrap'], function (browser, bs) {
     // http://www.vectoropenstock.com/previews/3437-Underwater-Vector-Background-.jpg
     var img_water = new Image();
     img_water.src = "images/water.jpg";
-
-    // sounds
-    var sound_bg, sound_crunch, sound_death, sound_success;
-    if(browser === "safari" || browser == "msie") {
-        /*
-        http://developer.apple.com/library/safari/#documentation/AudioVideo/Conceptual/Using_HTML5_Audio_Video/Device-SpecificConsiderations/Device-SpecificConsiderations.html
-
-        Safari does not support audio by default, so we'll make some dummy classes that don't break the rest of the game
-        
-        Also, Internet Explorer seems to struggle with the audio, crashing whenever setting the currentTime property of any sound.
-        */
-        function DummySound() {
-
-        }
-        DummySound.prototype.play = function () { }
-        DummySound.prototype.pause = function () { }
-        DummySound.prototype.addEventListener = function (param1, param2) { }
-        sound_bg = new DummySound();
-        sound_crunch = new DummySound();
-        sound_death = new DummySound();
-        sound_success = new DummySound();
-    } else {
-        sound_bg = document.createElement('audio');
-        sound_bg.setAttribute('src', "sounds/background.wav");  // buffers automatically when created
-        sound_bg.volume = 0.5;
-
-        sound_bg.addEventListener('ended', function() {
-            this.currentTime = 0;
-            this.play();
-        }, false);
-
-        sound_crunch = document.createElement('audio');
-        sound_crunch.setAttribute('src', "sounds/crunch.wav");
-        sound_crunch.volume = 1.0;
-        sound_death = document.createElement('audio');
-        sound_death.setAttribute('src', "sounds/death.wav");
-        sound_death.volume = 1.0;
-        sound_success = document.createElement('audio');
-        sound_success.setAttribute('src', "sounds/success.wav");
-        sound_success.volume = 1.0;
-    }
 
     // update loading screen when downloaded
     img_tick.onload = function() {
@@ -88,7 +43,6 @@ define(['_helpers/browser', 'bootstrap'], function (browser, bs) {
         bs.pubsub.emitEvent('regame:loaded:file');
     }
 
-
     return {
         img_tick: img_tick,
         img_cross: img_cross,
@@ -98,5 +52,4 @@ define(['_helpers/browser', 'bootstrap'], function (browser, bs) {
         img_sand: img_sand,
         img_water: img_water
     };
-
 });
