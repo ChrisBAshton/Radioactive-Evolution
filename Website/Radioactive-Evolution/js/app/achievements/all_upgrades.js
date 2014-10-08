33c92bfa324a891e93f6a98ab5213acef5e59807
ChrisBAshton <chrisashtonweb@gmail.com>
2014-10-08 19:18:24 +0100

    improved all upgrades

--- Website/Radioactive-Evolution/js/app/achievements/all_upgrades.js
+++ Website/Radioactive-Evolution/js/app/achievements/all_upgrades.js
@@ -1,4 +1,4 @@
-define(['bootstrap', 'achievements/_achievement'], function (bs, Achievement) {
+define(['bootstrap', 'achievements/_achievement', 'module/controller/upgrade_controller'], function (bs, Achievement, upgrades) {
 
     /**
     * An achievement gained by buying all possible game upgrades.
@@ -12,40 +12,19 @@ define(['bootstrap', 'achievements/_achievement'], function (bs, Achievement) {
         bs.extend(Achievement, this);
     	this.title="allUpgrades";
 
-        var allCamouflage = false,
-        	allFlyingFish = false,
-        	allGrow       = false,
-        	allMurkyWater = false,
-        	allPoison     = false;
-
-
-        // @TODO - come up with a less coupled way of checking all upgrades have been purchased.
         bs.pubsub.addListener('regame:upgrade:purchased', function (title, cost, currentLevel) {
-
-        	if (title === "Camouflage" && currentLevel === 1) {
-        		allCamouflage = true;
-        	}
-        	else if (title === "Flying Fish" && currentLevel === 1) {
-        		allFlyingFish = true;
-        	}
-        	else if (title === "Grow" && currentLevel === 4) {
-        		allGrow = true;
-        	}
-        	else if (title === "Murky Water" && currentLevel === 5) {
-        		allMurkyWater = true;
-        	}
-        	else if (title === "Poison" && currentLevel === 5) {
-        		allPoison = true;
-        	}
-
         	self.checkAchieved();
         });
 
         this.checkAchieved = function () {
 
-        	if (allCamouflage && allFlyingFish && allGrow && allMurkyWater && allPoison) {
-        		self.updateStatus(true);
-        	}
+            for(var i = 0; i < upgrades.upgrades.length; i++) {
+                if (upgrades.upgrades[i].canUpgrade()) {
+                    return false;
+                }
+            }
+
+        	self.updateStatus(true);
         }
     }
 
